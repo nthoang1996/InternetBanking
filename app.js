a7cfe17516c98c7ecb32b506b01eac764a8e52f1
nthoang <nthoang1996@gmail.com>
2020-05-22 08:50:51 +0700

    update cors

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express'); // call express
 const app = express(); // define our app using express
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 8000; // set our port
+app.use(cors())
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: false })); // support encoded bodies
+
 
 require('./middlewares/routes.mdw')(app);
 
--- routes/customer/behavior.route.js
+++ routes/customer/behavior.route.js
@@ -2,32 +2,15 @@ const express = require('express');
 const model = require('../../models/model')
 const config = require('../../config/config.json');
 const NodeRSA = require('node-rsa');
-const key = new NodeRSA(config.secret_key.private_key.join('\n'));
+const key = new NodeRSA(config.secret_key.public_key.join('\n'));
 key.setOptions({ encryptionScheme: 'pkcs1' });
 const router = express.Router();
-
 router.route('/recharge')
     .post(async function(req, res) {
-        const decrypted = key.decrypt(req.body.data, 'utf8');
-        console.log(decrypted);
-        const data = JSON.parse(decrypted);
-        console.log(typeof(data));
-        const source_id = data.source_id;
-        const des_id = data.des_id;
-        const value = data.value;
-        console.log(source_id, des_id, value);
-        const customer = await model.single_by_id('tbluser', des_id);
-        const cus_value = parseInt(customer[0].bank_balance) + parseInt(value);
-        const update_cus = await model.edit('tbluser', { bank_balance: cus_value }, { id: des_id });
-        const entity = {
-            type: 1,
-            source_id,
-            bank_company_id: req.headers.company_id,
-            des_id,
-            value,
-            message: ''
-        }
-        const insert_his = await model.add('tblhistorytransaction', entity);
+        let data = {};
+        console.log(req.body.data);
+        const verify = key.verify('req.body1', req.body.data,'', 'base64');
+        console.log(verify);
         res.status(200).json({ message: "Success" });
     })
 
